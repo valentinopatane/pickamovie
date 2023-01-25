72d7e91a089b78a0a5aa3b712b802c4ca5c6743d
Valentino Patane <valentinopatane98@gmail.com>
2023-01-24 23:16:17 -0300

    añadido extra esqueleto home

--- src/components/HomeFooter/HomeFooter.jsx
+++ src/components/HomeFooter/HomeFooter.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const HomeFooter = () => {
-    return <div className="homeFooter">HomeFooter</div>;
+    return (
+        <div className="homeFooter">
+            <p>
+                PICKAMOVIE.com -It’s a very simple tool that can help you
+                choose. It allows you to quickly decide what to watch so you can
+                jump straight up to the enterteinment. No registration is
+                required. The movies/series list is updated daily. Just open
+                pickamovie.com and be ready to watch movies, tv-shows and series
+                in a few seconds. We have almost any movie or tv-shows that you
+                will want to watch!
+            </p>
+
+            <div className="homeFooterCopy">
+                <span>PickaMovie. All Rights Reserved. 2023</span>
+                <h4>PickaMovie</h4>
+                <span>
+                    Feel free to support us by sharing with your friends and
+                    family
+                </span>
+            </div>
+        </div>
+    );
 };
 
 export default HomeFooter;
